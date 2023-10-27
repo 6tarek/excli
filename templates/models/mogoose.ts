6af3b0525a5efbe7feff3127b78dd6504be7984f
tarek ali <108217205+6tarek@users.noreply.github.com>
2023-10-27 19:52:32 +0300

    Create mogoose.ts

---
+++ templates/models/mogoose.ts
@@ -0,0 +1,8 @@
+import { Schema, InferSchemaType , model } from 'mongoose';
+
+const schema = new Schema({
+    
+  });
+  
+type thisiplaceHolderType = InferSchemaType<typeof schema>;
+export default model("thisiplaceHolder",thisiplaceHolderType)
